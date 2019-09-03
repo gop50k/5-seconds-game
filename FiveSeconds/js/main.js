7335dce07f2bb5aef7d31d7803642bf26a3198d5
Go nakagawa <g.nakagawa@gemcook.com>
2019-09-04 00:15:03 +0900

    js done

---
+++ FiveSeconds/js/main.js
@@ -0,0 +1,28 @@
+(function(){
+    'use strict';
+
+    var start = document.getElementById('start');
+    var stop = document.getElementById('stop');
+    var result = document.getElementById('result');
+    var startTime;
+
+    start.addEventListener('click', function() {
+        startTime = Date.now();
+    });
+
+    stop.addEventListener('click', function() {
+        var elapsedTime;
+        var diff;
+        elapsedTime = (Date.now() - startTime) / 1000;
+        result.textContent = elapsedTime.toFixed(3);
+        diff = elapsedTime - 5.0;
+        if (Math.abs(diff) < 1.0 ) {
+            result.className = 'perfect';
+        }
+    });
+
+
+
+})();
+
+
